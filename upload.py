c4cea7eadab9627e4d79474201f4b19cdbd6f63d
cmerrick-celonis <c.merrick@celonis.com>
2024-08-08 11:03:55 +0100

    first commit

---
+++ upload.py
@@ -0,0 +1,10 @@
+"""
+A set of functions to make uploading data via the PyCelonis API easier
+"""
+from pycelonis.service.integration.service import ColumnTransport
+from pandas import DataFrame
+
+def get_column_config(df:DataFrame):
+    """
+    """
+    pass
